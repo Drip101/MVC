8cd319d59c304e175547d59834a16aeb5ed9184b
Maddrick Long <110777599+Drip101@users.noreply.github.com>
2023-01-01 09:36:42 -0500

    update files and routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,71 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+//const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+
+        User
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((project) => post.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('all-post', {
+      posts
+      //logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        User,
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render('single-post', {
+      ...post,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+});
+
+module.exports = router;
