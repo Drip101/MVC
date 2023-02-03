f3078699314642e5c7d4c09db34a71501fe338aa
Maddrick Long <110777599+Drip101@users.noreply.github.com>
2023-02-03 00:48:38 -0500

    Update postRoutes.js

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,20 +1,15 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
 
 app.get('/', function(req, res) {
     res.send('Hello Sir')
 })
 
-    var express = require('express')
-var app = express()
 
 app.all('/', function(req, res) {
     console.log('Hello Sir')
     next()   // Pass the control to the next handler
 })
-
-var express = require('express')
-var app = express()
 
 app.post('/', function(req, res) {
     res.send('Hello Sir'))
