7b647a5a2620bf965abb38be64e70e4cc793288f
Maddrick Long <110777599+Drip101@users.noreply.github.com>
2023-02-03 10:59:14 -0500

    Update commentRoutes.js

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router()
+const withAuth =require('../../utils/auth')
+const {Comment} = require('../../models')
+
+router.post('/', withAuth, function(req, res) {
+    const body = req.body
+    Comment.create({...body, userId: req.session.userId})
+    .then(newComment=>{res.json(newComment)})
+    .catch(err =>{res.status(500).json(err)})
+})
